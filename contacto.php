f999ead30308fdb334e73f447b9f67baa307acfc
Pasantía Fintech <pasantia@iMac-de-Fernando.local>
2019-02-20 17:38:34 -0300

    primerisimo primer commit

---
+++ contacto.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>fintech.works</title>
+	
+	<link rel="shortcut icon" type="image/x-icon" href="https://static1.squarespace.com/static/575758912fe131ca9ef5a1ff/t/5765ac80d1758e42b25363a8/favicon.ico">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="style_basics.css" />
+	<link rel="image_src" href="http://static1.squarespace.com/static/575758912fe131ca9ef5a1ff/t/5765ac3a893fc027263765cf/1466281019378/logo-07-1.png?format=1500w" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="js" href="js/main.js">
+	<link rel="stylesheet" href="footer_style.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+
+
+</head>
+<body>
+	<div class="tablet">
+		<div class="circulo" style="z-index: 3; opacity: 1;height: 90px">
+			<a href="index.html"> <img src="logo.png" width="auto" height="50" style="position: fixed;"> </a>
+			<nav class="navbar" style="position: fixed;z-index: 3; right: 5%;">
+			<ul>
+				<a href="#" >
+					<li>
+						<img src="menu3.png" width="40" height="40" style="margin: 0px 0px 0px 0px;"> 
+						<ul class="dropdown-menu" style="margin-top: auto;right: 0%;">
+							<a href="platapp.html"><li>Platapp</li></a>
+							<a href="fn.html"><li>Finhealth</li></a>
+							<a href="banca.html"><li>Banca Web</li></a>
+							<a href="contacto.php"><li>Contactanos</li></a>
+						</ul>
+					</li>
+				</a>
+			</ul>
+			</nav>
+		</div>
+		<div class="fondo-img" style="height: 500px"><br><br><br><br><br><br>
+			<div class="texto-contenedor-1" style="text-align: left;color: white;margin-left: 56%;">
+				Contactanos 
+			</div>
+			<p class="texto" style="color: white;margin-left: 55%;">Una forma rapida de consultas y solisitudes <br>de forma facil y sencilla.</p>
+					
+		</div>
+		<br><br><br>
+		 
+
+	</div>
+
+	<div class="movil">
+		<div class="circulo" style="z-index: 3; height: 90px;">
+			<a href="index.html"> <img src="logo.png" width="auto" height="50" style="position: fixed;"> </a>
+			<nav class="navbar" style="position: fixed;z-index: 3; right: 5%;">
+			<ul>
+				<a href="#" >
+					<li>
+						<img src="menu3.png" width="40" height="40" style="margin: 0px 0px 0px 0px;"> 
+						<ul class="dropdown-menu" style="margin-top: auto;right: 0%;">
+							<a href="platapp.html"><li>Platapp</li></a>
+							<a href="fn.html"><li>Finhealth</li></a>
+							<a href="banca.html"><li>Banca Web</li></a>
+							<a href="contacto.php"><li>Contactanos</li></a>
+						</ul>
+					</li>
+				</a>
+			</ul>
+			</nav>
+		</div>
+	</div>
+		
+
+	<div class="contenedor">
+
+	<div class="barra">
+		
+		
+		<span style="margin: 0px 15% 0px 0px;"></span><a href="index.html"> <img src="logo.png" height="65" width="130"> </a>
+		
+		<div id="header">
+			<ul class="nav">
+				<li><a href="platapp.html">Platapp</a></li>
+				<li><a href="fn.html">Finhealth</a></li>
+				<li><a href="banca.html">Banca Web</a></li>
+				<li><a href="contacto.php" style="background-color:#4F8FDD;">Contactanos</a></li>
+			</ul>
+		</div>
+	
+
+	</div>
+
+	<div class="fondo-img" ><br><br><br><br><br><br><br><br><br><br><br>
+		<center>
+		<table align="right" style="width: 70%;">
+			<tr>
+		    	<th>
+
+					<p class="texto-contenedor-1" style="text-align: left;color: white;margin-left: 45%;display: inline-block;min-width: 400px;">
+					Contactanos </p>
+					<p class="texto" style="color: white;margin-left: 46.5%;min-width: 325px;">Una forma rapida de consultas y solisitudes <br>de forma facil y sencilla.</p>
+				</th>
+
+				<th>
+					<img src="like.png">
+
+				</th>
+			</tr>
+		</table>
+		</center>
+	</div>
+	
+		<br><br><br>
+		
+	</div>
+
+	<div class="contact1" >
+		
+
+		<div class="container-contact1">
+			<div class="contact1-pic js-tilt" data-tilt>
+				<img src="correo.png" alt="IMG">
+			</div>
+			<form class="contact1-form validate-form" method="POST">
+				<span class="contact1-form-title">
+					No te arrepentiras!
+				</span>
+
+				<div class="wrap-input1 validate-input" data-validate = "Se requiere de nombre">
+					<input class="input1" type="text" name="nombre" placeholder="Nombre Y Apellido" required>
+					
+				</div>
+
+				<div class="wrap-input1 validate-input" data-validate = "se requiere de telefono/celular: ex@abc.xyz">
+					<input class="input1" type="text" name="telefono" placeholder="Telefono/Celular" required>
+					
+				</div>
+
+				<div class="wrap-input1 validate-input" data-validate = "Se requiere email valido: ex@abc.xyz">
+					<input class="input1" type="email" name="correo" placeholder="Email" required>
+					
+				</div>
+
+				<div class="wrap-input1 validate-input" >
+					<select class="input1" style="border: none;" name="producto">
+					  	<option value="Cuestion">&nbsp;&nbsp;&nbsp;&nbsp;Producto de interes</option>
+					  	<option value="platapp">&nbsp;&nbsp;&nbsp;&nbsp;Platapp</option>
+					  	<option value="finhealth">&nbsp;&nbsp;&nbsp;&nbsp;Finhealth</option>
+					 	<option value="banca web">&nbsp;&nbsp;&nbsp;&nbsp;Banca Web</option>
+					</select>
+				</div>
+
+				<div class="wrap-input1 validate-input" data-validate = "Se requiere de un mensaje">
+					<textarea class="input1" name="mensaje" placeholder="Mensaje" required></textarea>
+					
+				</div>
+
+				<div class="container-contact1-form-btn" >
+					<input type='hidden' value='mayala@fintech.works' name='receptor'>
+					<button class="contact1-form-btn"style="background-color: #0C8FE8;">
+						<span>
+							Enviar correo
+							
+						</span>
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+
+
+	<footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<h3><img src="logo.png" width="200" height="100"></h3>
+
+				<p class="footer-links">
+					
+					<a href="#">PLATAPP</a>
+					·
+					<a href="#">FINHEALTH</a>
+					·
+					<a href="#">BANCA WEB</a>
+					·
+					<a href="#">CONTACTENOS</a>
+					
+				</p>
+
+				<p class="footer-company-name">fintech.works &copy; 2019</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Aviadores del Chaco 2050 - Torre:3 Piso:11</span> Asuncion, Paraguay</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+595 981 123 456</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:mayala@fintech.works">mayala@fintech.works</a></p>
+				</div>
+
+			</div>
+
+			
+
+		</footer>
+</div>
+
+<script>
+$(document).on("ready", function() {
+	$('ul li:has(ul)').click(function(e) {
+		$(".dropdown-menu").not($("ul", this)).slideUp("fast")
+		.next()
+		$(this).find('ul').slideToggle("fast")
+		.end();
+	});
+});
+
+</script>
+
+</body>
+</html>
+
+<?php 
+
+error_reporting(0);
+
+$nombre=$_POST["nombre"];
+$correo=$_POST["correo"];
+$tel=$_POST["telefono"];
+$prod=$_POST["producto"];
+$mensaje=$_POST["mensaje"];
+$r=$_POST["receptor"];
+
+
+$texto="Nombre y Apellido:".$nombre."\r\n";
+$texto.="Correo:".$correo."\r\n";
+$texto.="Telefono/Celular:".$tel."\r\n";
+$texto.="Producto de interes:".$prod."\r\n";
+$texto.="Mensaje:".$mensaje."\r\n";
+
+mail($r, $prod , $texto);
+
+//!'·$%&/()()==?¿¿*^¨Ç_:ÑÑ¨_:>!!'	|@/()()==?¿¿*^¨Ç#¢∞¬÷“”“”≠≠”“÷¬∞¬÷”≠´[∏~–…~{~–…–~…„ }¨Ç_:/()()==?¿¿*^¨ÇÑÑ¨_:>!!'	|@#¢∞¬÷“”“”≠≠”“÷¬∞¬÷”≠´[]\
+ ?>
